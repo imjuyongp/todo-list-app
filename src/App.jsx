9b3b4e4957d683d4e381c6b0d97f741da08c6afe
imjuyongp <imjuyongp@gmail.com>
2026-01-13 13:02:42 +0900

    Eiditor component 기능구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,49 @@ import Header from "./components/Header"
 import Editor from './components/Editor'
 import List from './components/List'
 
+import { useState, useRef } from 'react'
+
+const mockData = [
+    {
+      id : 0,
+      isDone : false,
+      content : "React study",
+      date : new Date().getTime(),
+    },
+    {
+      id : 1,
+      isDone : false,
+      content : "spring study",
+      date : new Date().getTime(),
+    },
+    {
+      id : 2,
+      isDone : false,
+      content : "kafka study",
+      date : new Date().getTime(),
+    },
+  ];
+
 function App() {
+  const [todos, setTodos] = useState(mockData)
+  const idRef = useRef(3)
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id : idRef.current++,
+      isDone : false,
+      content : content,
+      date : new Date().getTime(),
+    }
+
+    setTodos([newTodo, ...todos])
+
+  }
 
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate}/>
       <List />
     </div>
   )
